992ecdc1a96925032286cfe9503c029d570234d5
smriti.islur <smriti.islur@helpshift.com>
2025-03-06 12:27:45 +0530

    Make exception for connecting to db in core.clj

--- src/twitter/core.clj
+++ src/twitter/core.clj
@@ -2,12 +2,17 @@
   (:require [ring.adapter.jetty :as jetty]
             [clojure.pprint]
             [twitter.routes :as routes]
-            [twitter.db :as db]))
+            [twitter.db :as db]
+            [clojure.tools.logging :as log]))
 
 
 (defn -main
   [& args]
   (println "Starting server...")
-  db/start-connection
-  (jetty/run-jetty routes/app {:port 3000
-                               :join? false}))
+  (try
+    ;; Try to start the connection
+    (db/start-connection)
+    (jetty/run-jetty routes/app {:port 3000 :join? false})
+    (catch Exception e
+      ;; Catch and handle any connection or server start errors
+      (log/error "Error starting the server:" (.getMessage e)))))
--- src/twitter/db.clj
+++ src/twitter/db.clj
@@ -2,30 +2,30 @@
   (:require [monger.core :as mg]
             [monger.db :as mg-db]
             [monger.query :as mq]
-            [twitter.common :as comm])
-  (:import (java.io PushbackReader)))
+            [twitter.common :as comm]
+            [slingshot.slingshot :refer [try+ throw+]]
+            [clojure.tools.logging :as log]))
 
 
 (println "Mongo host from comm/config: " (:mongo-host comm/config))
 
 
-(def start-connection
+(defn start-connection []
   ;; Connect to the MongoDB server (default localhost:27017)
   (mg/connect [{:host (:mongo-host comm/config), :port (:mongo-port comm/config)}]))
 
 
-(defn get-db []
-  (mg/get-db start-connection (:mongo-db comm/config)))
-
-
-(defn list-collections []
-  (let [db (get-db)]
-    (mg-db/get-collection-names db)))
+;test
+(comment (def db-connect (start-connection))
+         (defn get-db []
+           (mg/get-db db-connect (:mongo-db comm/config)))
+         (defn list-collections []
+           (let [db (get-db)]
+             (mg-db/get-collection-names db)))
 
 
 ;; lists collections from the 'twitter' db
-(println "Collections in twitter DB:" (list-collections))
+         (println "Collections in twitter DB:" (list-collections))
+         ;; disconnect from MongoDB after you are done
+         (mg/disconnect db-connect))
 
-
-;; disconnect from MongoDB after you are done
-;; (mg/disconnect start-connection)
