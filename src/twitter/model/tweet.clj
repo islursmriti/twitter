b9bb899807a851e5ef7e6aef7d0d6ed3b3aba95b
smriti.islur <smriti.islur@helpshift.com>
2025-03-25 13:50:48 +0530

    Add post tweets route and corresponding function in xhr, handler and model

--- src/twitter/model/tweet.clj
+++ src/twitter/model/tweet.clj
@@ -1,1 +1,18 @@
-(ns twitter.model.tweet)
+(ns twitter.model.tweet
+  (:require
+   [twitter.utils :as utils]
+   [twitter.db :as db]
+   [twitter.model.user :refer [get-user]]))
+
+
+(defn post-tweet
+  [text media {:keys [id username]}]
+  (get-user id username) ;to check if user is active
+  (let [tweet-id (str (:uuid-prefix-tweet utils/data)
+                      (random-uuid))
+        current-time (java.util.Date/from (java.time.Instant/now))]
+    (db/insert (:mongo-coll-tweets utils/data)
+               {:id tweet-id :user-id id
+                :body {:text text :media media}
+                :created-at current-time :updated-at current-time})
+    tweet-id))
--- src/twitter/xhr/tweet.clj
+++ src/twitter/xhr/tweet.clj
@@ -1,1 +1,23 @@
-(ns twitter.xhr.tweet)
+(ns twitter.xhr.tweet
+  (:require
+   [twitter.common :as comm]
+   [twitter.status-codes :refer [http-code]]
+   [twitter.handler.tweet :as tht]))
+
+
+(defn post-tweet
+  [tweet user-data]
+  (try (let [tweet-id (tht/post-tweet tweet user-data)]
+         (comm/json-response (http-code :ok)
+                             {:message "success" :tweet-id tweet-id}))
+       (catch Exception e
+         (cond (= (.getMessage e) "parameter-validation-failed")
+               (comm/json-response (http-code :bad-request)
+                                   {:message "Text Or Media is Mandatory"}
+                                   (.getMessage e))
+               (= (.getMessage e) "user-doesnt-exists")
+               (comm/json-response (http-code :bad-request)
+                                   {:message "User Doesn't Exists"}
+                                   (.getMessage e))
+               :else (comm/json-response (:internal-server-error http-code)
+                                         {:message "Internal Server Error"})))))
